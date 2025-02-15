4d30b7cdedcf2f9e82d73aee975f0c42fc98c475
aran8276 <aran8276@gmail.com>
2025-02-15 20:43:29 +0700

    add home stage 2

--- src/app/(loggedin)/layout.tsx
+++ src/app/(loggedin)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-center py-6">{children}</main>
+      <>{children}</>
       <footer>Footer goes here</footer>
     </>
   );
